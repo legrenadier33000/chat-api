999d76f5a8819a31e2578a1f4d36fda3dac69884
John Doe <john.doe@acme.org>
2022-04-08 14:58:25 +0200

    AuthGuard now uses custom error types

--- src/middlewares/auth/authGuard.js
+++ src/middlewares/auth/authGuard.js
@@ -1,31 +1,39 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
+
+const InvalidAuthorizationHeader = require("../../utils/errors/InvalidAuthorizationHeader")
+const InvalidBearer = require("../../utils/errors/InvalidBearer")
+const InvalidJsonWebToken = require("../../utils/errors/InvalidJsonWebToken")
 
 const APP_SECRET_KEY = process.env.APP_SECRET_KEY
 
 const authguard = async (req, res, next) => {
-    try {
-        const authHeader = req.headers?.authorization
-        
-        if(!authHeader) { throw new Error("No Authorization header") }
-
-        const array = authHeader.split(' ')
-
-        if(array.length !== 2) { throw new Error("Invalid Authorizaiton header") } 
-
-        const bearer = array[0]
-        const token = array[1]
-
-        if(bearer !== "Bearer") { throw new Error("Invalid Bearer type")}
-
-        const decoded = jwt.verify(token, APP_SECRET_KEY)
-
-        if(!decoded) { throw new Error("Invalid JWT") }
-        
-        res.locals.user = decoded
-        return next()
-    } catch (e) {
-        res.status(403).send(e.message)
+    const authHeader = req.headers?.authorization
+    
+    if(!authHeader) { 
+        return next(InvalidAuthorizationHeader.factory())
     }
+
+    const array = authHeader.split(' ')
+
+    if(array.length !== 2) { 
+        return next(InvalidAuthorizationHeader.factory())
+    } 
+
+    const bearer = array[0]
+    const token = array[1]
+
+    if(bearer !== "Bearer") { 
+        return next(InvalidBearer.factory())
+    }
+
+    const decoded = jwt.verify(token, APP_SECRET_KEY)
+
+    if(!decoded) { 
+        return next(InvalidJsonWebToken.factory())
+    }
+    
+    res.locals.user = decoded
+    next()
 }
 
 module.exports = authguard
